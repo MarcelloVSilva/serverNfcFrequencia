b8b0f4badcf364a8fa8ca03338f78719379f12b4
MarcelloVSilva <mvnota10@gmail.com>
2017-11-22 20:01:23 -0200

    serverTeste

--- python/server.py
+++ python/server.py
@@ -1,17 +1,33 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+import os
 import socket
-import xmlrpclib
-from SimpleXMLRPCServer import SimpleXMLRPCServer
+from flask import Flask
+app = Flask(__name__)
 
-host = socket.gethostbyname(socket.gethostname())
-server = SimpleXMLRPCServer((host, 1235))
-print host, "Server conectado a porta 1235..."
+@app.route("/")
+def hello():
+    return "Hello from Python!"
 
-def getDadosTeacher():
-    print ("passou aqui")
-    return 1
+if __name__ == "__main__":
+    host = socket.gethostbyname(socket.gethostname())
+    port = int(os.environ.get("PORT", 2000))
+    app.run(host=host, port=port)
 
-server.register_function(getDadosTeacher, "getDadosTeacher")
-server.serve_forever()
+#
+#import socket
+#import xmlrpclib
+#from SimpleXMLRPCServer import SimpleXMLRPCServer
+#
+#host = socket.gethostbyname(socket.gethostname())
+#server = SimpleXMLRPCServer((host, 2000))
+#print host, "Server conectado a porta 2000..."
+#
+#def getDadosTeacher():
+#    print ("passou aqui")
+#    return 1
+#
+#server.register_function(getDadosTeacher, "getDadosTeacher")
+#server.serve_forever()
+#

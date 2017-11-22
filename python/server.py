fbc792963535c7d46efb83c5e8a9e39324970ade
MarcelloVSilva <mvnota10@gmail.com>
2017-11-22 01:44:35 -0200

    teste

--- python/server.py
+++ python/server.py
@@ -5,7 +5,7 @@ import xmlrpclib
 from SimpleXMLRPCServer import SimpleXMLRPCServer
 
 server = SimpleXMLRPCServer(("189.63.92.253", 1235))
-print "Server conectado a porta 1235..."
+#print "Server conectado a porta 1235..."
 
 def getDadosTeacher():
     print "passou aqui"

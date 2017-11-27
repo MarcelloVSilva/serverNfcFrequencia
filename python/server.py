f8882c483663716d05918bc0bcdbaf6a852501d0
MarcelloVSilva <mvnota10@gmail.com>
2017-11-27 21:00:02 -0200

    server

--- python/server.py
+++ python/server.py
@@ -1,33 +1,42 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-import os
 import socket
-from flask import Flask
-app = Flask(__name__)
+import xmlrpclib
+from SimpleXMLRPCServer import SimpleXMLRPCServer
+import json
 
-@app.route("/")
-def hello():
-    return "Hello from Python!"
+jsonTeacher = json.dumps(
+    {
+        'teacher':{
+            'idTeacher': 'marcelloId',
+            'nameTeacher': 'Marcello Victor',
+            'classrooms':[
+                {
+                    'id': 'sistemasDistribuidos2M23',
+                    'description': 'Sistemas distribuidos',
+                    'polygon': {}
+                }
+            ]
+        }
+    }, separators=(',',':'))
+print jsonTeacher
 
-if __name__ == "__main__":
-    host = socket.gethostbyname(socket.gethostname())
-    port = int(os.environ.get("PORT", 2000))
-    app.run(host=host, port=port)
+host = "172.16.118.97" # socket.gethostbyname(socket.gethostname())
+server = SimpleXMLRPCServer((host, 2000))
+print host, "Server conectado a porta 2000..."
 
-#
-#import socket
-#import xmlrpclib
-#from SimpleXMLRPCServer import SimpleXMLRPCServer
-#
-#host = socket.gethostbyname(socket.gethostname())
-#server = SimpleXMLRPCServer((host, 2000))
-#print host, "Server conectado a porta 2000..."
-#
-#def getDadosTeacher():
-#    print ("passou aqui")
-#    return 1
-#
-#server.register_function(getDadosTeacher, "getDadosTeacher")
-#server.serve_forever()
-#
+def getInfoTeacher(idTeacher):
+    print idTeacher
+    return jsonTeacher
+
+server.register_function(getInfoTeacher, "getInfoTeacher")
+server.serve_forever()
+
+
+
+
+                # ['-16.604074', '-49.264518'],
+                # ['-16.604206', '-49.264484'],
+                # ['-16.604320', '-49.264900'],
+                # ['-16.604183', '-49.264949'],
